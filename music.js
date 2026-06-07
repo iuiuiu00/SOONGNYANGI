0c6e8e36adfa97d2f8103c3c206215a0410aee40
jakewithhub <jake0411jm@gmail.com>
2026-06-08 01:06:13 +0900

    Update music.js

--- music.js
+++ music.js
@@ -1,1 +1,24 @@
+// music.js
+let bgm;
 
+function preloadMusic() {
+  soundFormats('mp3');
+  bgm = loadSound('assets/Void_Lantern.mp3');
+}
+
+function playBGM() {
+  if (bgm && !bgm.isPlaying()) {
+    bgm.setVolume(0.5);
+    bgm.loop();
+  }
+}
+
+function stopBGM() {
+  if (bgm && bgm.isPlaying()) {
+    bgm.stop();
+  }
+}
+
+function setBGMVolume(v) {
+  if (bgm) bgm.setVolume(v);
+}
